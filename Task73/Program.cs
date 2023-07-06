a35f4371ac091b23926cc3fb10f9805eaa96b5d2
Юра <yusckorobogatov@yandex.ru>
2023-07-06 21:00:45 +0400

    Решение задач на семинаре 8 и домашняя задача 56 в Task73

---
+++ Task73/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите 
+// программу, которая будет находить строку с наименьшей суммой элементов
+
+Console.Clear();
+Console.WriteLine("Введите количество строк: ");
+int rows = int.Parse (Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов: ");
+int columns = int.Parse (Console.ReadLine()!);
+
+
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = rnd.Next(min, max+1);
+        }
+    }
+    return result;
+}
+
+    static void PrintArray(int[,] array)
+        {
+            for (int a = 0; a < array.GetLength(0); a++)
+            {
+                for (int b = 0; b < array.GetLength(1); b++)
+                {
+                    Console.Write(array[a, b] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+      
+             List<int> sums = new List<int>();
+   
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                int sum = 0;
+                
+                
+                for (int j = 0; j < array.GetLength(1); j++) 
+                {
+                
+                    sum += array[i, j];
+                    
+                    
+                }
+                sums.Add(sum);
+                
+                Console.Write(sum + " ");
+               
+             }
+            Console.WriteLine("Суммы по строкам ");
---
+++ Task75/Program.cs
@@ -0,0 +1,109 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите 
+// программу, которая будет находить строку с наименьшей суммой элементов
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаб_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+            double sum = 0;
+            int m,n;
+            bool f;
+            do
+            {
+                Console.Write("Введите количество строк m: ");
+                f = int.TryParse(Console.ReadLine(), out m);
+                if (f == false)
+                    Console.WriteLine("Ошибка! Введите еще раз.");
+            }
+            while (!f);
+
+            do
+            {
+                Console.Write("Введите количество столбцов n: ");
+                f = int.TryParse(Console.ReadLine(), out n);
+                if (f == false)
+                    Console.WriteLine("Ошибка! Введите еще раз.");
+            }
+            while (!f);
+
+
+            double[,] mas = new double[n,m];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    double x = rnd.Next(-33531, 33110);
+                    x = x / 100.000;
+                    mas[i, j] = x;
+                }
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Массив: ");
+            
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("{0,8:F3}\t", mas[i, j]);
+                }
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Сумма каждой строки: ");
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = 0; j < n; j++)
+                {
+                    sum += Math.Abs(mas[i,j]);
+                }
+              Console.WriteLine("{0} - sum = {1:F3}",i, sum);
+              sum = 0;
+          
+            }
+           
+
+
+            double swap;
+            for (int c = 0; c <= (m * n); c++)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n - 1; j++)
+                    {
+                        if (mas[i, j] > mas[i, j + 1])
+                        {
+                            swap = mas[i, j];
+                            mas[i, j] = mas[i, j + 1];
+                            mas[i, j + 1] = swap;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("Отсортированный массив: ");
+           
+            for (int i = 0; i < n; i++)
+            {
+                
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("{0,8:F3}\t",  mas[i, j]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
